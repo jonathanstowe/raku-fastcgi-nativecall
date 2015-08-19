235033cfe175963cd10b59e192ce0bc0e188dffd
Carlin <cb@viennan.net>
2015-08-20 10:09:29 +1200

    add build module for panda installation

---
+++ Build.pm
@@ -0,0 +1,20 @@
+use v6;
+use Panda::Common;
+use Panda::Builder;
+use Shell::Command;
+
+class Build is Panda::Builder {
+	method build($dir) {
+		my Str $ext = "$dir/ext";
+		my Str $blib = "$dir/blib";
+		rm_f("$ext/fcgi.so");
+		rm_rf($blib);
+		mkdir($blib);
+		mkdir("$blib/lib");
+		my $here = $*CWD;
+		chdir($ext);
+		shell("make");
+		chdir($here);
+		cp("$ext/fcgi.so", "$blib/lib/fcgi.so");
+	}
+}
